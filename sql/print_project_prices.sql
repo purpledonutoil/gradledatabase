8fff3b8b7b078e758e90036cdabf2d41fc582918
purpledonutoil <purpledonutoil@gmail.com>
2025-02-21 12:09:51 +0100

    Removed all files

--- sql/print_project_prices.sql
+++
@@ -1,6 +0,0 @@
-SELECT project_id, SUM(salary*DATEDIFF(MONTH, start_date, finish_date)) AS PRICE  
-FROM worker
-JOIN project_worker ON worker.id = project_worker.worker_id
-JOIN project ON project_worker.project_id = project.id
-GROUP BY project_id
-ORDER BY PRICE DESC;
